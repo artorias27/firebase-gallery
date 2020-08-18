8510b73d953c6ca35a066021c4467a8bbcb05d28
John-Sang-Pi <superkiller498@gmail.com>
2020-08-18 16:37:26 +0630

    Inject image-gallery

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
 import Title from './comps/Title';
 import UploadForm from './comps/UploadForm';
-import ImageGrid from './comps/ImageGrid';
-import Modal from './comps/Modal';
+import useFirestore from './hooks/useFirestore';
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
 function App() {
   const [selectedImg, setSelectedImg ] = useState(null);
-
+  const photos = useFirestore("Wallpapers").map( ({ src, width, height }) => ({ src, width, height }) )
+  console.log(photos)
   return (
     <div className="App">
       <Title/>
       <UploadForm />
-      <ImageGrid setSelectedImg={setSelectedImg}></ImageGrid>
-      { selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} /> }
+      <Gallery photos={photos} />
+      {/* <ModalGateway>
+        <Modal>
+          <Carousel />
+        </Modal>
+      </ModalGateway> */}
     </div>
   );
 }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,14 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-  };
+  apiKey: "AIzaSyATFcSGYR2-7JzSb8gbkMWL7MdWphQWbok",
+  authDomain: "react-gallery-f66cf.firebaseapp.com",
+  databaseURL: "https://react-gallery-f66cf.firebaseio.com",
+  projectId: "react-gallery-f66cf",
+  storageBucket: "react-gallery-f66cf.appspot.com",
+  messagingSenderId: "138325170415",
+  appId: "1:138325170415:web:7fa6e40c2e43fcf501631a"
+};
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
